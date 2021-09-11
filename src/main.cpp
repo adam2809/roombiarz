27196d61565b55c9eacf7df40c49e90ca3e529e5
andek <adam.kule@gmail.com>
2021-09-11 21:21:20 +0200

    Adds basic motor control functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include "pinout.h"
 
+enum motor_t{
+	MOTOR_A,
+	MOTOR_B
+};
+
 void setup_driver_pins(){
 	pinMode(ENA_DRIVER_PIN, OUTPUT);
 	pinMode(ENB_DRIVER_PIN, OUTPUT);
@@ -13,12 +18,61 @@ void setup_driver_pins(){
 	digitalWrite(IN2_DRIVER_PIN, LOW);
 	digitalWrite(IN3_DRIVER_PIN, LOW);
 	digitalWrite(IN4_DRIVER_PIN, LOW);
+
+	digitalWrite(ENA_DRIVER_PIN, LOW);
+	digitalWrite(ENB_DRIVER_PIN, LOW); 
 }
 
-void setup() {
+void setup() {  
+	Serial.begin(9600); 
+
 	setup_driver_pins();
 }
 
+void set_motor_forward(motor_t motor){
+	if (motor == MOTOR_A){
+		digitalWrite(IN1_DRIVER_PIN, LOW);
+		digitalWrite(IN2_DRIVER_PIN, HIGH);
+	}else{
+		digitalWrite(IN3_DRIVER_PIN, LOW);
+		digitalWrite(IN4_DRIVER_PIN, HIGH);
+	}
+}
+
+
+void set_motor_backward(motor_t motor){
+	if (motor == MOTOR_A){
+		digitalWrite(IN1_DRIVER_PIN, HIGH);
+		digitalWrite(IN2_DRIVER_PIN, LOW);
+	}else{
+		digitalWrite(IN3_DRIVER_PIN, HIGH);
+		digitalWrite(IN4_DRIVER_PIN, LOW);
+	}
+}
+
+void set_motor_stopped(motor_t motor){
+	if (motor == MOTOR_A){
+		digitalWrite(IN1_DRIVER_PIN, LOW);
+		digitalWrite(IN2_DRIVER_PIN, LOW);
+	}else{
+		digitalWrite(IN3_DRIVER_PIN, LOW);
+		digitalWrite(IN4_DRIVER_PIN, LOW);
+	}
+}
+
 void loop() {
+	digitalWrite(ENA_DRIVER_PIN, HIGH);
+
+	Serial.println("Motor Forward");
+	set_motor_forward(MOTOR_A);
+	delay(2000);
+
+	Serial.println("Motor stopped");	
+	set_motor_stopped(MOTOR_A);
+	delay(2000);
+
+	Serial.println("Motor backward");	
+	set_motor_backward(MOTOR_A);
+	delay(2000);
 
 }
