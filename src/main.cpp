2f2547d6988b1ec6510cf0380ae49c9992c9ab79
andek <adam.kule@gmail.com>
2021-09-12 16:43:29 +0200

    Adds led reaction to proximity functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <Arduino.h>
 #include "pinout.h"
+namespace prox_sensor_led_pin{
+    enum pin_t{
+        PROX_SENSOR_LED_CENTER = 8,
+        PROX_SENSOR_LED_RIGHT = 9,
+        PROX_SENSOR_LED_LEFT = 10
+    };
 
+    static const pin_t all[] = { 
+        PROX_SENSOR_LED_CENTER,
+        PROX_SENSOR_LED_RIGHT,
+        PROX_SENSOR_LED_LEFT 
+    };
+}
 enum motor_t{
 	MOTOR_A,
 	MOTOR_B
@@ -72,9 +84,18 @@ void set_motor_speed(motor_t motor,int speed){
 int get_prox(prox_sensor_pin_t pin){
 	float volts = analogRead(pin)*0.0048828125;
 	int distance = 13*pow(volts, -1); 
-    return distance <= 30 ? distance : -1;
+    return distance <= MAX_PROX_VALUE ? distance : -1;
 }
 
 void loop() {
+	for (int i=0;i<PROX_SENSOR_COUNT;i++){
+		int dist = get_prox(all_prox_sensors[i]);
+		if(dist == -1){
+			digitalWrite(all_led_pins[i],LOW);
+		}else{
+			int led_brightness = map(dist,0,MAX_PROX_VALUE,255,50);
+			analogWrite(all_led_pins[i],led_brightness);
+		}
+	}
 	Serial.println(get_prox(PROX_SENSOR_CENTER));
 }
--- src/pinout.h
+++ src/pinout.h
@@ -8,8 +8,29 @@ enum driver_pin_t{
     ENB_DRIVER_PIN = 2
 };
 
+#define MAX_PROX_VALUE 30
+#define PROX_SENSOR_COUNT 3
 enum prox_sensor_pin_t{
     PROX_SENSOR_CENTER = A0,
     PROX_SENSOR_RIGHT = A1,
     PROX_SENSOR_LEFT = A2
 };
+
+enum prox_sensor_led_pin_t{
+    PROX_SENSOR_LED_CENTER = 8,
+    PROX_SENSOR_LED_RIGHT = 9,
+    PROX_SENSOR_LED_LEFT = 10
+};
+
+prox_sensor_led_pin_t all_led_pins[] = {
+    PROX_SENSOR_LED_CENTER,
+    PROX_SENSOR_LED_RIGHT,
+    PROX_SENSOR_LED_LEFT
+};
+
+prox_sensor_pin_t all_prox_sensors[] = {
+    PROX_SENSOR_CENTER,
+    PROX_SENSOR_RIGHT,
+    PROX_SENSOR_LEFT
+};
+
